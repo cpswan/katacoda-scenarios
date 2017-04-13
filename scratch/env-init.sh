296908a065aa461093ee3f857c80a31b364710e4
Chris Swan <@cpswan>
2017-04-13 17:23:52 +0100

    Do I need double quotes?
    Signed-off-by: Chris Swan <chrisswan@dxc.com>

--- scratch/env-init.sh
+++ scratch/env-init.sh
@@ -1,11 +1,11 @@
 mkdir tasks
-echo '# ./ansible-nginx/tasks/install_nginx.yml' > tasks/install_nginx.yml
-echo '- name: NGINX | Installing NGINX' >> tasks/install_nginx.yml
-echo '  apt:' >> tasks/install_nginx.yml
-echo '    name: nginx' >> tasks/install_nginx.yml
-echo '    state: latest' >> tasks/install_nginx.yml
-echo '' >> tasks/install_nginx.yml
-echo '- name: NGINX | Starting NGINX' >> tasks/install_nginx.yml
-echo '  service:' >> tasks/install_nginx.yml
-echo '    name: nginx' >> tasks/install_nginx.yml
-echo '    state: started' >> tasks/install_nginx.yml
+echo "# tasks/install_nginx.yml" > tasks/install_nginx.yml
+echo "- name: NGINX | Installing NGINX" >> tasks/install_nginx.yml
+echo "  apt:" >> tasks/install_nginx.yml
+echo "    name: nginx" >> tasks/install_nginx.yml
+echo "    state: latest" >> tasks/install_nginx.yml
+echo "" >> tasks/install_nginx.yml
+echo "- name: NGINX | Starting NGINX" >> tasks/install_nginx.yml
+echo "  service:" >> tasks/install_nginx.yml
+echo "    name: nginx" >> tasks/install_nginx.yml
+echo "    state: started" >> tasks/install_nginx.yml
