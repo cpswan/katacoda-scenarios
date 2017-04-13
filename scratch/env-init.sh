a17f6eb7583697f57f3859d701984c65f65ed29b
Chris Swan <@cpswan>
2017-04-13 17:13:50 +0100

    Use cat <<EOF for install_nginx.yml
    Signed-off-by: Chris Swan <chrisswan@dxc.com>

--- scratch/env-init.sh
+++ scratch/env-init.sh
@@ -1,2 +1,20 @@
 mkdir tasks
-mv install_nginx tasks
+cat << EOF > tasks/install_nginx.yml
+# ./ansible-nginx/tasks/install_nginx.yml
+
+#- name: NGINX | Installing NGINX repo rpm
+#  yum:
+#    name: http://nginx.org/packages/rhel/7/noarch/RPMS/nginx-release-rhel-7-0.el7.ngx.noarch.rpm
+
+- name: NGINX | Installing NGINX
+  apt:
+    name: nginx
+    state: latest
+
+- name: NGINX | Starting NGINX
+  service:
+    name: nginx
+    state: started
+
+# Playbook ends here
+EOF
