a0bc470345adb785022ba1b3984c05698bfcbcf8
Chris Swan <@cpswan>
2017-04-17 15:56:54 +0100

    Jenkins in Docker
    Signed-off-by: Chris Swan <@cpswan>

--- scratch/assets/README.md
+++
@@ -1,5 +0,0 @@
-Demo Ansible playbook to install nginx on the container host instance.
-
-Based on the NGINX example [Installing NGINX and NGINX Plus With Ansible](https://www.nginx.com/blog/installing-nginx-nginx-plus-ansible/)
-
-Modified to use become.
--- scratch/env-init.sh
+++ scratch/env-init.sh
@@ -1,11 +1,1 @@
-mkdir tasks
-echo "# tasks/install_nginx.yml" > tasks/install_nginx.yml
-echo "- name: NGINX | Installing NGINX" >> tasks/install_nginx.yml
-echo "  apt:" >> tasks/install_nginx.yml
-echo "    name: nginx" >> tasks/install_nginx.yml
-echo "    state: latest" >> tasks/install_nginx.yml
-echo "" >> tasks/install_nginx.yml
-echo "- name: NGINX | Starting NGINX" >> tasks/install_nginx.yml
-echo "  service:" >> tasks/install_nginx.yml
-echo "    name: nginx" >> tasks/install_nginx.yml
-echo "    state: started" >> tasks/install_nginx.yml
+docker run -p 8080:8080 -p 50000:50000 jenkins
