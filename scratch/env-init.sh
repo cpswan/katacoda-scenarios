e88afe5c06641a93b3dda6ba8fdca3d9e9483249
Chris Swan <@cpswan>
2017-04-17 16:27:12 +0100

    Named container
    Signed-off-by: Chris Swan <@cpswan>

--- scratch/env-init.sh
+++ scratch/env-init.sh
@@ -1,2 +1,2 @@
 mkdir ~/jenkins_home
-docker run -p 8080:8080 -p 50000:50000 -v ~/jenkins_home:/var/jenkins_home jenkins:2.46.1-alpine
+docker run -p 8080:8080 -p 50000:50000 --name jenkins jenkins:2.46.1-alpine
